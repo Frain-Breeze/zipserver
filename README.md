9b0d18a00c7117015af6e277fa31545f169f5f02
Frain <frainbreezeyt@gmail.com>
2021-05-29 11:30:56 +0200

    add readme n stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+# zipserver
+a basic FTP server which shows .zip, .rar and .7z files as folders, allowing you to browse them without first having to download the archive.
+the server is read-only, so there's no renaming or uploading or anything like that.
+login hasn't been implemented, so any username + password combo will be seen as valid.
+there's also plenty of exploits lurking here and there, which I might or might not patch up later.
--- src/main.cpp
+++ src/main.cpp
@@ -14,33 +14,49 @@ int main(int argc, char* argv[]) {
 		int port = 2121;
 		std::unordered_map<std::string, fs::path> root_points;
 
-		std::ifstream settings("ftp_settings.txt");
-		char buffer[2048];
-		while (settings.getline(buffer, 2048)) {
-			std::string bufstr = buffer;
-			size_t found = bufstr.find_first_of(' ');
-			if (found != std::string::npos) {
-				if (bufstr.substr(0, found) == "PORT") {
-					sscanf(bufstr.substr(found, std::string::npos).c_str(), "%d", &port);
-				}
-				else if (bufstr.substr(0, found) == "FOLDER") {
-					std::string first, second;
-					std::string todo = bufstr.substr(found, std::string::npos);
-					size_t offset = 0;
-					bool instring = false;
-					bool insecond = false;
-					while (offset != todo.length()) {
-						if (todo[offset] == '\"') {
-							if (instring) { instring = false; insecond = true; }
-							else { instring = true; }
-						}
-						else if (instring && insecond) { second += todo[offset]; }
-						else if (instring && !insecond) { first += todo[offset]; }
-						offset++;
+		if (fs::exists("ftp_settings.txt") && fs::is_regular_file("ftp_settings.txt")) {
+			std::ifstream settings("ftp_settings.txt");
+			char buffer[2048];
+			while (settings.getline(buffer, 2048)) {
+				std::string bufstr = buffer;
+				size_t found = bufstr.find_first_of(' ');
+				if (found != std::string::npos) {
+					if (bufstr.substr(0, found) == "PORT") {
+						sscanf(bufstr.substr(found, std::string::npos).c_str(), "%d", &port);
 					}
-					root_points.insert_or_assign(first, fs::u8path(second));
+					else if (bufstr.substr(0, found) == "FOLDER") {
+						std::string first, second;
+						std::string todo = bufstr.substr(found, std::string::npos);
+						size_t offset = 0;
+						bool instring = false;
+						bool insecond = false;
+						while (offset != todo.length()) {
+							if (todo[offset] == '\"') {
+								if (instring) { instring = false; insecond = true; }
+								else { instring = true; }
+							}
+							else if (instring && insecond) { second += todo[offset]; }
+							else if (instring && !insecond) { first += todo[offset]; }
+							offset++;
+						}
+						root_points.insert_or_assign(first, fs::u8path(second));
+					}
 				}
 			}
+		}
+		else {
+			FILE* sett = fopen("ftp_settings.txt", "wb");
+			if (sett) {
+				std::string def = "PORT 2121\nFOLDER \"server_directory\" \"" + fs::current_path().u8string() + "\"\n";
+				fwrite(def.c_str(), 1, strlen(def.c_str()), sett);
+				fclose(sett);
+				printf("unable to find an existing settings file, creating one with default settings.\n");
+			}
+			else {
+				printf("unable to find an existing settings file, and can't open a new one. starting server from current folder instead\n");
+			}
+
+			root_points.insert_or_assign("server_directory", fs::current_path().u8string());
 		}
 
 		asio::io_context io_context;
@@ -50,7 +66,7 @@ int main(int argc, char* argv[]) {
 		io_context.run();
 	}
 	else {
-		printf("not enough arguments, or too many! we need 0, we have %d\n", argc - 1);
+		printf("too many arguments! we don't want any\n");
 		std::cin.ignore();
 	}
 }
